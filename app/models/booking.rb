910f06a7069cee9b59eb146861751e4f55cf676c
cloteau camille <camille.cloteau@gmail.com>
2022-02-21 18:05:49 +0100

    destroy booking and plant model setup with device

--- app/models/booking.rb
+++
@@ -1,8 +0,0 @@
-class Booking < ApplicationRecord
-  belongs_to :plant
-  belongs_to :user
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
