f8cc3a876385866c484c798c7e3091a5f9f1347f
anna gatland <anna.g.gatland@gmail.com>
2022-02-22 17:56:01 +0100

    Better version for navbar in different contexts
    Use application.html.erb to render navbar component based on controller & action + introduce helper to abstract away the if/else logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def context_for_navbar
+    if params[:controller] == 'pages' && params[:action] == 'home'
+      :homepage
+    elsif params[:controller] == 'plants' && params[:action] == 'index'
+      :sticky
+    else
+      :classic
+    end
+  end
 end
