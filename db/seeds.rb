9c6b1edcfa2cc795dab54c5764e5e9f9c9362d51
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-02-22 16:08:20 +0100

    New seed file with updated data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,9 @@ require "open-uri"
 
 ## USERS
 # User.create(email: , password: , first_name: , last_name: , address: )
-user1 = User.create(
+puts "Creating users:"
+
+user1 = User.new(
   email: "john@mail.com",
   password: "secret",
   first_name: "John",
@@ -19,8 +21,10 @@ user1 = User.create(
 )
 avatar1 = URI.open('https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=3&w=500&h=500&q=80')
 user1.avatar.attach(io: avatar1, filename: "jonh-avatar.jpg", content_type: 'image/jpg')
+user1.save
+puts "   > saved user1"
 
-user2 = User.create(
+user2 = User.new(
   email: "brenda@mail.com",
   password: "secret",
   first_name: "Brenda",
@@ -29,8 +33,10 @@ user2 = User.create(
 )
 avatar2 = URI.open('https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=3&w=500&h=500&q=80')
 user2.avatar.attach(io: avatar2, filename: "brenda-avatar.jpg", content_type: 'image/jpg')
+user2.save
+puts "   > saved user2"
 
-user3 = User.create(
+user3 = User.new(
   email: "emma@mail.com",
   password: "secret",
   first_name: "Emma",
@@ -39,8 +45,10 @@ user3 = User.create(
 )
 avatar3 = URI.open('https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=3&w=500&h=500&q=80')
 user3.avatar.attach(io: avatar3, filename: "emma-avatar.jpg", content_type: 'image/jpg')
+user3.save
+puts "   > saved user3"
 
-user4 = User.create(
+user4 = User.new(
   email: "michael@mail.com",
   password: "secret",
   first_name: "Michael",
@@ -49,8 +57,10 @@ user4 = User.create(
 )
 avatar4 = URI.open('https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=3&w=500&h=500&q=80')
 user4.avatar.attach(io: avatar4, filename: "michael-avatar.jpg", content_type: 'image/jpg')
+user4.save
+puts "   > saved user4"
 
-user5 = User.create(
+user5 = User.new(
   email: "satia@mail.com",
   password: "secret",
   first_name: "Satia",
@@ -59,11 +69,15 @@ user5 = User.create(
 )
 avatar5 = URI.open('https://images.unsplash.com/photo-1493106819501-66d381c466f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=3&w=500&h=500&q=80')
 user5.avatar.attach(io: avatar5, filename: "-avatar.jpg", content_type: 'image/jpg')
+user5.save
+puts "   > saved user5"
 
+
+puts "Creating plants:"
 
 ## PLANTS
-# Plant.create(name: , description: , day_price: , user: )
-plant1 = Plant.create(
+# Plant.new(name: , description: , day_price: , user: )
+plant1 = Plant.new(
   name: "Bambous",
   description: "Floraison parfumée Non
   Le genre Fargesia est originaire des régions du sud et du sud-ouest de la Chine au-dessus de 1000 m d'altitude. Scabrida est l'une des espèces, introduite en Europe fin 1990. 'Asian Wonder' est une variété horticole récente issue de la culture in vitro. Cette variété de bambou moyen non-traçant (cespiteux), atteint 4 m de hauteur pour une largeur de 3 m. Feuilles vertes, très fines. Chaumes verts de 1 cm de diamètre, devenant violacés avec l'âge. Pousse dans un sol fertile, frais de préférence. Exposition au soleil ou à mi-ombre. Très rustique, jusqu'à -28 °C. Jardinière : 60 cm.
@@ -80,8 +94,10 @@ file12 = URI.open('https://images.unsplash.com/photo-1440342359743-84fcb8c21f21?
 plant1.photos.attach(io: file12, filename: 'bamboo2.jpg', content_type: 'image/jpg')
 file13 = URI.open('https://images.unsplash.com/photo-1601385304565-9a38a051a1a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80')
 plant1.photos.attach(io: file13, filename: 'bamboo3.jpg', content_type: 'image/jpg')
+plant1.save
+puts "   > saved plant1"
 
-plant2 = Plant.create(
+plant2 = Plant.new(
   name: "Cupressus Sempervirens Totem",
   description: "Hauteur à maturité (cm) 900
   Résistance au froid -15 à -20 °C
@@ -112,8 +128,10 @@ file22 = URI.open('https://images.unsplash.com/photo-1624356417152-a1069f183130?
 plant2.photos.attach(io: file22, filename: 'cypress2.jpg', content_type: 'image/jpg')
 file23 = URI.open('https://images.unsplash.com/photo-1552732053-dbd3141436a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y3lwcmVzcyUyMHRyZWV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60')
 plant2.photos.attach(io: file23, filename: 'cypress3.jpg', content_type: 'image/jpg')
+plant2.save
+puts "   > saved plant2"
 
-plant3 = Plant.create(
+plant3 = Plant.new(
   name: "Rosier buisson 'Jules Verne'",
   description: "Hauteur à maturité (cm) 100
   Résistance au froid -15 à -20 °C
@@ -157,8 +175,39 @@ file32 = URI.open('https://images.unsplash.com/photo-1549480557-be950919d61a?ixl
 plant3.photos.attach(io: file32, filename: 'rose_bush2.jpg', content_type: 'image/jpg')
 file33 = URI.open('https://images.unsplash.com/photo-1617386416437-ec6e778580fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80')
 plant3.photos.attach(io: file33, filename: 'rose_bush3.jpg', content_type: 'image/jpg')
+plant3.save
+puts "   > saved plant3"
 
-plant5 = Plant.create(
+plant4 = Plant.new(
+  name: "Magnolia",
+  description: "Hauteur à maturité (cm) 0
+  Résistance au froid -20 à -25 °C
+  Arrosage Arrosez-le régulièrement et copieusement les deux premières années, le temps qu'il s'installe. Sans excès non plus : car les racines craignent l'humidité stagnante. Paillez pour limiter les arrosages
+  Floraison parfumée Non
+  Mode de vie Vivace
+  Les magnolias forment un genre de plantes à fleurs, de la famille des Magnoliacées, qui comprend environ cent dix espèces, essentiellement des arbres et arbustes, des régions tempérées chaudes. Il existe deux grands types de Magnolias : les magnolias à feuilles caduques, qui fleurissent au début du printemps, avant l'apparition des feuilles, et le magnolia à grandes fleurs, qui a des feuilles persistantes et porte de grandes fleurs, de 20-25 cm de diamètre, plus grandes que les précédentes, en été. Leur floraison est dans tous les cas très belle (grands pétales clairs). Leurs feuilles sont grandes et vertes.
+  Truffaut conseille : Offrez-lui un emplacement abrité des courants d'air froids.
+  Truffaut informe : Le magnolia préfère les sols frais, riches, profonds, mais très bien drainés. Il n'a pas besoin d'un sol forcément acide pour prospérer. Une terre neutre convient également.
+  Plante méditerranéenne : Non
+  Mode d'utilisation : Isolé
+  Couleur du feuillage : Vert
+  Feuillage décoratif : Oui
+  Lieu de vie : Plante d'extérieur
+  Exposition : Soleil,Mi-ombre
+  Type de sol : Frais",
+  day_price: 8.8,
+  user: User.all.sample
+)
+file41 = URI.open('https://images.unsplash.com/photo-1554067241-c7fade64cba7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODl8fG1hZ25vbGlhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60')
+plant4.photos.attach(io: file41, filename: 'magnolia1.jpg', content_type: 'image/jpg')
+file42 = URI.open('https://images.unsplash.com/photo-1619255191114-6df1f2922120?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80')
+plant4.photos.attach(io: file42, filename: 'magnolia2.jpg', content_type: 'image/jpg')
+file43 = URI.open('https://images.unsplash.com/photo-1580851015909-8c16dbeaf513?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fG1hZ25vbGlhJTIwdHJlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60')
+plant4.photos.attach(io: file43, filename: 'magnolia3.jpg', content_type: 'image/jpg')
+plant4.save
+puts "   > saved plant4"
+
+plant5 = Plant.new(
   name: "Kumquat ",
   description: "Le kumquat donne des petits fruits orangés (3 à 4 cm) dont la saveur est un peu acide, mais agréable. Le fruit se consomme en entier, avec sa peau, qui est très fine et légèrement sucrée. De croissance lente, le kumquat forme un petit arbre au port assez compact qui atteint environ 3 mètres de haut en pleine terre. Ses petites feuilles ovales sont d'un beau vert foncé brillant. Le kumquat entre en dormance (repos végétatif) d'octobre à mai, sa floraison et sa fructification sont donc plus tardives que celles des Citrus.
   Truffaut conseille :  Un kumquat adulte, à l'abri du vent, peut supporter des gels jusque -8°C voir -10°C pour quelques heures. Mais les fruits gèlent à partir de -4°C. Le kumquat peut donc être cultivé en pleine terre dans les régions de climat doux, avec un paillage en hiver. Ailleurs, il sera cultivé en bac. Ainsi, vous pourrez l'hiverner dans une pièce lumineuse et hors gel (serre froide ou véranda non chauffée). Si le bac reste en extérieur, entourez le contenant d'un plastique à bulle et la plante d'un voile d'hivernage. Le kumquat aime le soleil, les sols légers, humifères, bien drainés et non calcaires. Prévoir un lit de billes d'argile au fond du pot ou du trou de plantation, pour faciliter l'évacuation de l'eau excédentaire. Les fruits très décoratifs ont besoin d'une longue période de chaleur pour arriver à maturité.
@@ -173,8 +222,10 @@ file52 = URI.open('https://images.unsplash.com/photo-1558412143-264fe5144c8c?ixl
 plant5.photos.attach(io: file52, filename: 'kumquat2.jpg', content_type: 'image/jpg')
 file53 = URI.open('https://images.unsplash.com/photo-1621632614995-fd64bb166db8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8a3VtcXVhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60')
 plant5.photos.attach(io: file53, filename: 'kumquat3.jpg', content_type: 'image/jpg')
+plant5.save
+puts "   > saved plant5"
 
-plant6 = Plant.create(
+plant6 = Plant.new(
   name: "Passiflora caerulea ",
   description: "Hauteur à maturité (cm) 800
   Résistance au froid -5 à -10 °C
@@ -211,47 +262,10 @@ file62 = URI.open('https://images.unsplash.com/photo-1628341423248-4b8c5c51a3cd?
 plant6.photos.attach(io: file62, filename: 'plantphoto2.jpg', content_type: 'image/jpg')
 file63 = URI.open('https://images.unsplash.com/photo-1633557534989-f7cbb576d7e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHBhc3NpZmxvcmElMjBjYWVydWxlYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60')
 plant6.photos.attach(io: file63, filename: 'plantphoto3.jpg', content_type: 'image/jpg')
+plant6.save
+puts "   > saved plant6"
 
-plant6 = Plant.create(
-  name: "Lavandula angustifolia",
-  description: "Hauteur à maturité (cm) 60
-  Résistance au froid -10 à -15 °C
-  Feuillage Persistant
-  Arrosage Arrosage copieux à la plantation. Le pire ennemi de la lavande est l'excès d'humidité. Plantation dans un terrain bien drainé.
-  Floraison de Juin à Juillet
-  Floraison parfumée Oui
-  Mode de vie Vivace
-  Le genre Lavandula comprend 25 espèces, originaires du Sud de la France, de la péninsule Ibérique, des îles Canaries et Baléares. Lavandula angustifolia est l'une de ces espèces, originaire des basses montagnes méditerranéennes. Cet arbuste, à forme buissonnante arrondie, à croissance rapide, atteint 60 cm de hauteur pour une largeur de 60 cm. Feuillage persistant, grisâtre puis vert. Feuilles étroites à bords incurvés, de 3 à 5 cm. Fleurs violettes, en épis terminaux, en juin-juillet. Cultivée pour extraction du parfum. Pousse dans un sol normal, même sec et calcaire. Exposition chaude et ensoleillée. Taille après la floraison. On le plante en isolé, en massif, en bordures, plates-bandes, haies basses fleuries. Recommandée pour la culture en bac.
-  Truffaut conseille : Vous pouvez réaliser une belle bordure avec cette lavande à la floraison violette, très parfumée, en juin-juillet, et presque sans arrosage
-  Truffaut informe : Attention, variétés et coloris peuvent changer selon disponibilités et arrivages (Hidcote Blue, Grosso, Dutch, Rosea, Twickel Purple, Dwarf Blue, Edelweiss..). Tous les 3 à 4 ans, supprimer à la base les branches les plus anciennes pour rajeunir la plante.
-  Plante méditerranéenne : Non
-  Mode d'utilisation : Isolé,Massif,Bordure,Haie,En pot
-  Port de la plante : Arrondi compact
-  Feuillage : Persistant
-  Couleur du feuillage : Vert
-  Feuillage décoratif : Oui
-  Hauteur à maturité (cm) : 60
-  Largeur moyenne à maturité : 60
-  Dimension moyenne à maturité (cm) : H 60 L 60 cm
-  Lieu de vie : Plante d'extérieur
-  Exposition : Soleil
-  Type de sol : Normal,Sec
-  Période de floraison : de Juin à Juillet
-  Forme de la fleur : En épis
-  Couleur de floraison : Violet
-  Floraison parfumée : Oui
-  Type de parfum : Floral",
-  day_price: 3.2,
-  user: User.all.sample
-)
-file61 = URI.open('https://images.unsplash.com/photo-1499002238440-d264edd596ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80')
-plant6.photos.attach(io: file61, filename: 'lavender1.jpg', content_type: 'image/jpg')
-file62 = URI.open('https://images.unsplash.com/photo-1600759487717-62bbb608106e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80')
-plant6.photos.attach(io: file62, filename: 'lavender2.jpg', content_type: 'image/jpg')
-file63 = URI.open('https://images.unsplash.com/photo-1541927634837-a7d5c4892527?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80')
-plant6.photos.attach(io: file63, filename: 'lavender3.jpg', content_type: 'image/jpg')
-
-plant7 = Plant.create(
+plant7 = Plant.new(
   name: "Eucalyptus gregsoniana",
   description: "Hauteur à maturité (cm) 400
   Résistance au froid -10 à -15 °C
@@ -288,8 +302,10 @@ file72 = URI.open('https://images.unsplash.com/photo-1607183199003-cf994ad22ed8?
 plant7.photos.attach(io: file72, filename: 'eucalyptus2.jpg', content_type: 'image/jpg')
 file73 = URI.open('https://images.unsplash.com/photo-1593924056246-356bb1684f47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=700&q=60')
 plant7.photos.attach(io: file73, filename: 'eucalyptus3.jpg', content_type: 'image/jpg')
+plant7.save
+puts "   > saved plant7"
 
-plant8 = Plant.create(
+plant8 = Plant.new(
   name: "Olea Cipressino Nord",
   description: "Floraison parfumée Non
   Idéal à cultiver en haie, brise-vent ou clôture, l'olivier cipressino (ou frangivento) se caractérise par sa forme pyramidale. Cette variété toscane offre une bonne résistance au froid et s'adapte très facilement. C'est un arbre fruitier qui produit des olives violacées, permettant la production d’huile d’olive.
@@ -305,8 +321,10 @@ file82 = URI.open('https://images.unsplash.com/photo-1629797278949-f75067a28371?
 plant8.photos.attach(io: file82, filename: 'olive_tree2.jpg', content_type: 'image/jpg')
 file83 = URI.open('https://images.unsplash.com/photo-1627706146186-bb11f3d00f5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80')
 plant8.photos.attach(io: file83, filename: 'olive_tree3.jpg', content_type: 'image/jpg')
+plant8.save
+puts "   > saved plant8"
 
-plant9 = Plant.create(
+plant9 = Plant.new(
   name: "Cycas revoluta",
   description: "Hauteur à maturité (cm) 400
   Résistance au froid -5 à -10 °C
@@ -341,3 +359,46 @@ file92 = URI.open('https://images.unsplash.com/photo-1484506097116-1bcba4fa7568?
 plant9.photos.attach(io: file92, filename: 'palm_tree2.jpg', content_type: 'image/jpg')
 file93 = URI.open('https://images.unsplash.com/photo-1615821598181-85caf305a078?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTU5fHxwYWxtJTIwdHJlZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60')
 plant9.photos.attach(io: file93, filename: 'palm_tree3.jpg', content_type: 'image/jpg')
+plant9.save
+puts "   > saved plant9"
+
+plant10 = Plant.new(
+  name: "Lavandula angustifolia",
+  description: "Hauteur à maturité (cm) 60
+  Résistance au froid -10 à -15 °C
+  Feuillage Persistant
+  Arrosage Arrosage copieux à la plantation. Le pire ennemi de la lavande est l'excès d'humidité. Plantation dans un terrain bien drainé.
+  Floraison de Juin à Juillet
+  Floraison parfumée Oui
+  Mode de vie Vivace
+  Le genre Lavandula comprend 25 espèces, originaires du Sud de la France, de la péninsule Ibérique, des îles Canaries et Baléares. Lavandula angustifolia est l'une de ces espèces, originaire des basses montagnes méditerranéennes. Cet arbuste, à forme buissonnante arrondie, à croissance rapide, atteint 60 cm de hauteur pour une largeur de 60 cm. Feuillage persistant, grisâtre puis vert. Feuilles étroites à bords incurvés, de 3 à 5 cm. Fleurs violettes, en épis terminaux, en juin-juillet. Cultivée pour extraction du parfum. Pousse dans un sol normal, même sec et calcaire. Exposition chaude et ensoleillée. Taille après la floraison. On le plante en isolé, en massif, en bordures, plates-bandes, haies basses fleuries. Recommandée pour la culture en bac.
+  Truffaut conseille : Vous pouvez réaliser une belle bordure avec cette lavande à la floraison violette, très parfumée, en juin-juillet, et presque sans arrosage
+  Truffaut informe : Attention, variétés et coloris peuvent changer selon disponibilités et arrivages (Hidcote Blue, Grosso, Dutch, Rosea, Twickel Purple, Dwarf Blue, Edelweiss..). Tous les 3 à 4 ans, supprimer à la base les branches les plus anciennes pour rajeunir la plante.
+  Plante méditerranéenne : Non
+  Mode d'utilisation : Isolé,Massif,Bordure,Haie,En pot
+  Port de la plante : Arrondi compact
+  Feuillage : Persistant
+  Couleur du feuillage : Vert
+  Feuillage décoratif : Oui
+  Hauteur à maturité (cm) : 60
+  Largeur moyenne à maturité : 60
+  Dimension moyenne à maturité (cm) : H 60 L 60 cm
+  Lieu de vie : Plante d'extérieur
+  Exposition : Soleil
+  Type de sol : Normal,Sec
+  Période de floraison : de Juin à Juillet
+  Forme de la fleur : En épis
+  Couleur de floraison : Violet
+  Floraison parfumée : Oui
+  Type de parfum : Floral",
+  day_price: 3.2,
+  user: User.all.sample
+)
+file101 = URI.open('https://images.unsplash.com/photo-1499002238440-d264edd596ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80')
+plant10.photos.attach(io: file101, filename: 'lavender1.jpg', content_type: 'image/jpg')
+file102 = URI.open('https://images.unsplash.com/photo-1600759487717-62bbb608106e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80')
+plant10.photos.attach(io: file102, filename: 'lavender2.jpg', content_type: 'image/jpg')
+file103 = URI.open('https://images.unsplash.com/photo-1541927634837-a7d5c4892527?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80')
+plant10.photos.attach(io: file103, filename: 'lavender3.jpg', content_type: 'image/jpg')
+plant10.save
+puts "   > saved plant10"
